0cd73aa4a253d155c5a924e242809b7598506183
Julien Bernard <julien.bernard@univ-fcomte.fr>
2022-11-10 16:29:22 +0100

    first commit

---
+++ src/gamma.c
@@ -0,0 +1,10 @@
+/* gamma, GAMes Made with Agate
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2022 Julien Bernard
+ */
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  return EXIT_SUCCESS;
+}
