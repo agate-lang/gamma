9120fd44b12b7dbf08f96f2f561d48922969b06f
Julien Bernard <julien.bernard@univ-fcomte.fr>
2022-11-11 23:25:11 +0100

    init and shutdown SDL and Agate

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# gamma
-GAMes Mades with Agate
+# gamma, GAMes Mades with Agate
+
+gamma is a set of Agate units buit on top of [SDL](https://www.libsdl.org/) to easily make games with the Agate scripting language.
+
+
--- src/gamma.c
+++ src/gamma.c
@@ -4,7 +4,35 @@
  */
 #include <stdlib.h>
 
+#include <SDL2/SDL.h>
+
+#include "agate.h"
+#include "agate-support.h"
+
 int main(int argc, char *argv[]) {
+
+  // initialize SDL
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    return EXIT_FAILURE;
+  }
+
+  // initialize Agate
+
+  AgateConfig config;
+  agateConfigInitialize(&config);
+
+  AgateVM *vm = agateExNewVM(&config);
+
+
+  // shutdown Agate
+
+  agateExDeleteVM(vm);
+
+  // shutdown SDL
+
+  SDL_Quit();
 
   return EXIT_SUCCESS;
 }
